9da0668fb24d80d309d498181a5b4e9db6bec45f
NickCoding122 <nicholaswsymons@gmail.com>
2025-12-11 15:26:14 +0000

    Update import path for authOptions in AdminPage

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,4 +1,4 @@
-import { authOptions } from "@/lib/authOptions";
+import { authOptions } from "../../../lib/authOptions";
 import { getServerSession } from "next-auth";
 import Link from "next/link";
 
